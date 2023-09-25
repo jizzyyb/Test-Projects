d8e754453ca5b6a1e08d3ccf94df1b09d7ef69ae
jizzyyb <merdanakyuez@gmail.com>
2023-09-25 21:35:23 +0200

    worked on js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 // Array for questions and answers
 let questions = [
   {
-    question: "Was bedeutet HTML in ausgeschriebener form?",
-    answers: ["Hallo", "Was", "geht", "ab"],
+    question: "Wer hat HTML erfunden?",
+    answers: [
+      "Robbie Williams",
+      "Lady Gaga",
+      "Tim Berners-Lee",
+      "Justin Bieber",
+    ],
     right_answer: [3],
   },
 
   {
     question: "Was bedeutet HTML in ausgeschriebener form?",
     answers: ["1", "2", "3", "4"],
+    right_answer: [4],
   },
 
   {
     question: "Was bedeutet HTML?",
-    answers: ["1", "2", "3", "4"],
+    answers: ["a", "s", "d", "f"],
+    right_answer: [1],
   },
 
   {
     question: "Was bedeutet HTML in ausgeschriebener form?",
-    answers: ["1", "2", "3", "4"],
+    answers: ["111", "222", "333", "444"],
+    right_answer: [2],
   },
 ];
 
@@ -33,12 +41,54 @@ function init() {
 
 function showQuestion() {
   // Funktion zum Anzeigen der Fragen
+
+  if (curentQuestion >= questions.length) {
+  } else {
+    let question = questions[curentQuestion];
+    document.getElementById("nmbr-change").innerHTML = curentQuestion + 1;
+    document.getElementById("questiontext").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answers"][0];
+    document.getElementById("answer_2").innerHTML = question["answers"][1];
+    document.getElementById("answer_3").innerHTML = question["answers"][2];
+    document.getElementById("answer_4").innerHTML = question["answers"][3];
+  }
+}
+
+function answer(selection) {
   let question = questions[curentQuestion];
+  console.log("Selected Answer is", selection);
+  let selectedQuestionNumber = selection.slice(-1);
+  console.log("Selected Question Number is", selectedQuestionNumber);
+  console.log("Current question is", question["right_answer"]);
 
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answers"][0];
-  document.getElementById("answer_2").innerHTML = question["answers"][1];
-  document.getElementById("answer_3").innerHTML = question["answers"][2];
-  document.getElementById("answer_4").innerHTML = question["answers"][3];
+  let rightAnswer = `answer_${question["right_answer"]}`;
 
+  if (selectedQuestionNumber == question["right_answer"]) {
+    console.log("Richtige Antwort!");
+    document.getElementById(selection).classList.add("bg-success");
+  } else {
+    console.log("Falsch!");
+    document.getElementById(selection).classList.add("bg-danger");
+    document.getElementById(rightAnswer).classList.add("bg-success");
+  }
+  document.getElementById("next-btn").disabled = false;
+}
+
+function nextQuestion() {
+  curentQuestion++; // von 0 auf 1
+  document.getElementById("next-btn").disabled = true; // Next Page button sperren bis eine Antwort gefunden richtig/falsch
+  resetAnswers();
+  showQuestion();
+}
+
+function resetAnswers() {
+  // reset all answers after clicking on the next question
+  document.getElementById("answer_1").classList.remove("bg-danger");
+  document.getElementById("answer_1").classList.remove("bg-success");
+  document.getElementById("answer_2").classList.remove("bg-danger");
+  document.getElementById("answer_2").classList.remove("bg-success");
+  document.getElementById("answer_3").classList.remove("bg-danger");
+  document.getElementById("answer_3").classList.remove("bg-success");
+  document.getElementById("answer_4").classList.remove("bg-danger");
+  document.getElementById("answer_4").classList.remove("bg-success");
 }
